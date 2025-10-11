ec972a7e00b19aee723388ecdd7d50351ad3845f
oladeji olaoluwa <90112749+Boluex@users.noreply.github.com>
2025-10-11 14:12:38 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import AnalyticsDashboard from "./components/AnalyticsDashboard";  // ← ADD THIS LINE
+
 
 const queryClient = new QueryClient();
 
