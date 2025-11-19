02872c6890ff5a542f773aad580a29fa25497639
oladeji olaoluwa <90112749+Boluex@users.noreply.github.com>
2025-11-19 15:40:37 +0100

    Update DownloadSection.tsx

--- src/components/DownloadSection.tsx
+++ src/components/DownloadSection.tsx
@@ -194,7 +194,7 @@ export const DownloadSection = () => {
 
             <div className="mt-6 p-4 bg-yellow-50/50 border border-yellow-200 rounded-lg">
               <p className="text-sm text-yellow-800">
-                💡 No payment required! We'll email your service token within minutes.
+                💡 Send us a feedback over here <a>https://tally.so/r/mR7ydj</a>
               </p>
             </div>
           </div>
